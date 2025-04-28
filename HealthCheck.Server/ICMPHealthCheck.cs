2d20204530f96a9bc1884d7548a6f55d6bf30ac4
Ebraheem <ebraheemsaid7@gmail.com>
2025-04-28 06:45:18 +0300

    Add project files.

---
+++ HealthCheck.Server/ICMPHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.NetworkInformation;
+
+namespace HealthCheck.Server
+{
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        private readonly string Host = $"10.0.0.0";
+        private readonly int HealthyRoundTrip = 300;
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var ping = new Ping();
+                var reply = await ping.SendPingAsync(Host);
+
+                switch (reply.Status)
+                {
+                    case IPStatus.Success:
+                        return (reply.RoundtripTime > HealthyRoundTrip)
+                            ? HealthCheckResult.Degraded()
+                            : HealthCheckResult.Healthy();
+
+                    default:
+                        return HealthCheckResult.Unhealthy();
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
